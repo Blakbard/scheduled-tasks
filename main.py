12e4db7a053429b0123cfa2e93326b007325be5c
Blakbard <53714361+Blakbard@users.noreply.github.com>
2026-08-22 22:56:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas
 import smtplib
 
 MY_EMAIL = os.environ.get("MY_EMAIL")
-PASSWORD = os.environ.get("MY_PASSWORD")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 data= pandas.read_csv(r".\birthdays.csv")
 bday_dic= {(data_row.day,data_row.month):data_row for (index,data_row) in data.iterrows()}
@@ -22,6 +22,6 @@ if celebrant_bday in bday_dic:
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=MY_EMAIL,password=PASSWORD)
+        connection.login(user=MY_EMAIL,password=MY_PASSWORD)
         bday_messages= "".join(store_message)
         connection.sendmail(from_addr=MY_EMAIL,to_addrs=bday_dic[celebrant_bday]["email"],msg=f"Subject:HURRAY!!!\n\n{bday_messages}")
