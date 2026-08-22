e1b2df3ceb689ba73d58adb24937734245ce9e13
Blakbard <53714361+Blakbard@users.noreply.github.com>
2026-08-22 18:48:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
+import datetime as dt
 import random
-import smtplib
 import os
+import pandas
+import smtplib
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+data= pandas.read_csv(r".\birthdays.csv")
+bday_dic= {(data_row.day,data_row.month):data_row for (index,data_row) in data.iterrows()}
+now=dt.datetime.now()
+celebrant_bday= (now.day,now.month)
+store_message=[]
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+if celebrant_bday in bday_dic:
+    rand_template=random.choice(os.listdir(r".\letter_templates"))
+    with open(rf".\letter_templates\{rand_template}") as bday_file:
+        bday_message=bday_file.read()
+        new_bday_message= bday_message.replace("[NAME]", bday_dic[celebrant_bday]["name"])
+        store_message.append(new_bday_message)
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL,password=PASSWORD)
+        bday_messages= "".join(store_message)
+        connection.sendmail(from_addr=MY_EMAIL,to_addrs=bday_dic[celebrant_bday]["email"],msg=f"Subject:HURRAY!!!\n\n{bday_messages}")
